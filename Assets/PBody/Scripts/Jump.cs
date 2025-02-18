c2443bf3dc178024693c81d1ca912d631154e134
Travis m <murdocktravis03@gmail.com>
2025-02-18 09:59:44 -0800

    working player

--- Assets/PBody/Scripts/Jump.cs
+++ Assets/PBody/Scripts/Jump.cs
@@ -6,22 +6,24 @@ public class Jump : MonoBehaviour
 {
     public float jumpForce;
     private bool isOnGround = true;
-
     private Rigidbody2D rb;
-
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
-
-    // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
             isOnGround = false;
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
         }
     }
 }
